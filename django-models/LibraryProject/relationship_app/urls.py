96c355f24d83ab6e3b3355e35b3fa288ae0aa70c
fiyinfoluwa <fiyinfoluwaoyekan@gmail.com>
2025-02-23 21:36:11 +0100

    new stuff

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -15,5 +15,5 @@ urlpatterns = [
     # Authentication URLs
     path('login/', auth_views.LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
-    path('register/', register, name='register'),
+    path('register/', views.register, name='register'),
 ]
