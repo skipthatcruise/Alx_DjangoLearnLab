fa54c0adc610e234ba43f237855ceebd65ee1979
fiyinfoluwa <fiyinfoluwaoyekan@gmail.com>
2025-02-23 19:23:22 +0100

    library detail added

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -10,7 +10,7 @@ def library_list(request):
 
 class LibraryDetailView(DetailView):
     model = Library
-    template_name = "relationship_app/list_books.html"
+    template_name = "relationship_app/library_detail.html"
     context_object_name = "library"
 
     def get_context_data(self, **kwargs):
