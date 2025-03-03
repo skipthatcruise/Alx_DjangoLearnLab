e41057787cce04477635cc46a1c4e33a74147817
fiyinfoluwa <fiyinfoluwaoyekan@gmail.com>
2025-03-03 20:00:30 +0100

    admin.py updated

--- advanced_features_and_security/LibraryProject/bookshelf/admin.py
+++ advanced_features_and_security/LibraryProject/bookshelf/admin.py
@@ -1,10 +1,26 @@
 from django.contrib import admin
-from .models import Book
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser
 
-class BookAdmin(admin.ModelAdmin):
-    list_display = ('title', 'author', 'publication_year')
-    list_filter = ('title', 'author', 'publication_year')
-    search_fields = ('title', 'author', 'publication_year')
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser  # Specify the custom user model
+    list_display = ("email", "date_of_birth", "is_staff", "is_superuser")  # Fields visible in the list view
+    fieldsets = (
+        (None, {"fields": ("email", "password")}),  # Basic user details
+        ("Personal Info", {"fields": ("date_of_birth", "profile_photo")}),  # Custom fields
+        ("Permissions", {"fields": ("is_active", "is_staff", "is_superuser", "groups", "user_permissions")}),
+        ("Important Dates", {"fields": ("last_login", "date_joined")}),
+    )
+    add_fieldsets = (
+        (None, {
+            "classes": ("wide",),
+            "fields": ("email", "date_of_birth", "profile_photo", "password1", "password2"),
+        }),
+    )
+    ordering = ("email",)  # Order users by email
+    search_fields = ("email",)  # Enable search by email
 
-admin.site.register(Book, BookAdmin)
+admin.site.register(CustomUser, CustomUserAdmin)  # Register the custom user model
+
+
 
